5da53de1b5394225300cffd192741d17a785c29a
Henri-Rene Kuningas <henri.kuningas@finbite.eu>
2022-01-04 13:13:55 +0200

    33 included admin user list

--- app/core/models.py
+++ app/core/models.py
@@ -3,17 +3,28 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.conf import settings
 
-class BaseUserManager(BaseUserManager):
+class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """creates and saves new user"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def User(AbstractBaseUser, PermissionsMixin):
+    def create_superuser(self, email, password):
+        """ Creates and saves superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
         """Customized user model that supports using email instead of username """
         email = models.EmailField(max_length=255, unique=True)
         name = models.CharField(max_length=255)
